a639d12166661c7583a274819b462c56eca89779
Sebastian Cornejo <ibanez.am@gmail.com>
2016-07-18 21:26:05 -0400

    Octavo commit

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ActiveRecord::Base
 	belongs_to :category
 	has_one :inventories
-	has_many :works
+	has_many :works, dependent: :destroy
 	validates :item_size, numericality: {greater_than: 0}
 
 	scope :size, -> (n) {where(item_size: n)}
